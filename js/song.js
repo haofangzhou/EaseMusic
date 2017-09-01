3fe6f8d68aade3664062f16c70db91423bec8f85
haofangzhou <haofangzhou@sinosoft.com.cn>
2017-09-01 11:44:40 +0800

    增加播放和暂停功能

--- js/song.js
+++ js/song.js
@@ -3,14 +3,12 @@ $(function(){
         console.log("lyric=="+object)
         let aaa =JSON.parse(object);
         let lyric=aaa.lrc.lyric;
-        console.log("lyric=="+aaa.lrc.lyric)
         let arry=lyric.split('\n')
         // let regex=/^\[(.+)\](.+)/g
         let regex=/^\[(.+)\](.+)$/
         arry=arry.map(function(string,index){
             // let matches=regex.exec(string)
              let matches=string.match(regex)
-             console.log("333333="+matches);
             if(matches){
                 return {time:matches[1],words:matches[2]}
             }
@@ -19,7 +17,6 @@ $(function(){
         let $lyric =$('.lyric')
         arry.map(function(object){
             if(!object) {return}
-            console.log("sss="+object.time);
             let $p=$('<p/>')
             $p.attr('data-time',object.time).text(object.words)
             $p.appendTo($lyric.children('.lines'))
@@ -27,12 +24,17 @@ $(function(){
     })
 
     let audio =document.createElement('audio')
-    audio.src='http://m10.music.126.net/20170831152256/3dbd78088fa4beaa3b7934bc0675bcca/ymusic/2087/0e9a/89c5/3a7fe466f11c872349eb792e454f77c6.mp3'
+    audio.src='http://dl.stream.qqmusic.qq.com/C4000027zPYs3A9fyb.m4a?fromtag=38&vkey=AEE597A012F34B51A237B0F922C5A00863C2042F970A949D2A36C62FA535C772B34F487095E6F72B8343E2B86E921F6318F7A2B62370C2D9&guid=7490034320'
     audio.oncanplay = function(){
-        audio.play();
+    audio.play();
         $('.disc-container').addClass('playing')
     }
-
-
-
+    $('.pause').on('click',function(){
+        audio.pause()
+        $('.disc-container').removeClass('playing')
+    })
+    $('.play').on('click',function(){
+        audio.play()
+        $('.disc-container').addClass('playing')
+    })
 })
